673a940e02b49a5dff828b931561862229a44b68
Cord Harms <cord.harms@loproducts.de>
2026-03-17 17:44:46 +0100

    added karabiner config and created readme.

---
+++ README.md
@@ -0,0 +1,15 @@
+# dotfiles
+
+Personal dotfiles managed with stow.
+
+## Install
+```bash
+curl -fsSL https://raw.githubusercontent.com/droczy/.dotfiles/main/install.sh | bash
+```
+
+## Configs
+
+- aerospace - window manager
+- alacritty - terminal emulator
+- karabiner - keyboard remapping
+- zsh - shell configuration
--- install.sh
+++ install.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 set -e
 
+if [ ! -d "$HOME/.dotfiles" ]; then
+  echo "Cloning dotfiles..."
+  git clone git@github.com:droczy/.dotfiles.git ~/.dotfiles
+else
+  echo "Dotfiles already exist, pulling latest..."
+  git -C ~/.dotfiles pull
+fi
+
 echo "Installing stow..."
 brew install stow
 
-echo "Setting up dotfiles..."
+echo "Removing existing configs..."
 cd ~/.dotfiles
-stow --adopt .
+# Für jede Datei im Repo: lösche die auf dem System falls sie existiert
+stow --simulate . 2>&1 | grep "existing target" | awk '{print $NF}' | while read f; do
+  rm -rf "$HOME/$f"
+done
+
+echo "Setting up dotfiles..."
+stow .
 
 echo "Done!"
